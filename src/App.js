063390cd7dc5d471c22f3a5b522b323bc81d303a
Salih Alperen <sal.alperen1@gmail.com>
2022-06-28 23:27:49 +0200

    Added Shared Layout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
-import { Landing, Error, Dashboard, Register } from './pages';
+import { Landing, Error, Register } from './pages';
+import {
+  AddJob,
+  AllJobs,
+  Profile,
+  SharedLayout,
+  Stats,
+} from './pages/dashboard';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -6,7 +13,12 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Dashboard />} />
+        <Route path='/' element={<SharedLayout />}>
+          <Route index element={<Stats />} />
+          <Route path='alljobs' element={<AllJobs />} />
+          <Route path='addjob' element={<AddJob />} />
+          <Route path='profile' element={<Profile />} />
+        </Route>
         <Route path='landing' element={<Landing />} />
         <Route path='register' element={<Register />} />
         <Route path='*' element={<Error />} />
